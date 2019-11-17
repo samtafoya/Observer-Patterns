f32caf94c7acfa71b8000ec2d3cee7d9d8d8772b
samtafoya <sammit@live.com>
2019-11-17 11:46:02 -0800

    confirm working

---
+++ Observer-Patterns/src/MyMain.java
@@ -0,0 +1,9 @@
+public class MyMain {
+	public static void main(String[] args) {
+		//Create an observer window and a controller window.
+		//The controller window will be observed.
+		ObserverWindow observerWindow = new ObserverWindow();
+		new ControllerWindow(observerWindow);
+		// test
+	}
+}
